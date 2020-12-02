08b09a2cace64cef4b76e9e15e3e3db5d5b3ed04
Qing Sheng <xxxquillsq@gmail.com>
2020-12-02 12:07:49 +0000

    added routing for add,update,delete movie

--- movies-api/index.js
+++ movies-api/index.js
@@ -1,6 +1,7 @@
 import dotenv from 'dotenv';
 import express from 'express';
 import moviesRouter from './api/movies';
+import bodyParser from 'body-parser';
 
 dotenv.config();
 
@@ -9,6 +10,10 @@ const app = express();
 const port = process.env.PORT;
 
 app.use(express.static('public'));
+//configure body-parser
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
+
 app.use('/api/movies', moviesRouter);
 
 app.listen(port, () => {
